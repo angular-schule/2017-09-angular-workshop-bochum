9a0368dbcefaa0bc535060766f979e1e71fbb61d
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-09-18 19:21:08 +0200

    html5-playground: using webpack with bootstrap + sass

---
+++ html5-playground/src/js/main.js
@@ -0,0 +1,3 @@
+require("../sass/style.scss");
+
+alert('Hallo! ;-)')
---
+++ html5-playground/webpack.config.js
@@ -0,0 +1,38 @@
+// webpack.config.js
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    main: ['./js/main.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].min.js',
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // creates style nodes from JS strings
+          { loader: "style-loader" },
+          // translates CSS into CommonJS
+          { loader: "css-loader", options: { sourceMap: true } },
+          // compiles Sass to CSS
+          { loader: "sass-loader", options: { sourceMap: true } }
+        ]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: ['file-loader']
+      }]
+  },
+  resolve: {
+    modules: ['node_modules']
+  }
+};
