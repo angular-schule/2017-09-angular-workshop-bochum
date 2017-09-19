02ce047ea555efb0a50df0cddc0e69e77e873378
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-09-19 14:08:14 +0200

    adds boostrap@4.0.0-beta

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  books: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.books = ['Angular', 'AngularJS'];
   }
-
 }
