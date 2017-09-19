6d40ca9b915076e8b2b623423b0f19941b121b55
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-09-19 18:14:57 +0200

    Schreibfehler korrigiert

--- book-rating/src/app/shared/book.ts
+++ book-rating/src/app/shared/book.ts
@@ -2,7 +2,7 @@ export class Book {
   constructor(
     public isbn: string,
     public title: string,
-    public desciption: string,
+    public description: string,
     public rating = 0
   ) { }
 
