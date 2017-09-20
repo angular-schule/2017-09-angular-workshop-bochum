deebb0d9d56c4eb9d120bb2e83190cc50552d414
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-09-20 14:32:21 +0200

    Test für Datum im Dashboard

--- book-rating/src/app/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,8 @@
+import { LOCALE_ID, NO_ERRORS_SCHEMA } from '@angular/core';
+import { BookComponent } from '../book/book.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DatePipe } from '@angular/common';
 
 import { DashboardComponent } from './dashboard.component';
 
@@ -8,7 +12,11 @@ describe('DashboardComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        { provide: LOCALE_ID, useValue: 'de' }
+      ]
     })
     .compileComponents();
   }));
@@ -21,5 +29,17 @@ describe('DashboardComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the date', () => {
+    const dateBox = fixture.debugElement.query(By.css('.date-box')).nativeElement;
+
+
+    expect(dateBox.textContent)
+      .toContain(new DatePipe('de').transform(component.d, 'longDate'));
+
+    component.d = new Date('2017-07-15 00:00:00');
+    fixture.detectChanges();
+    expect(dateBox.textContent).toContain('15. Juli 2017');
   });
 });
